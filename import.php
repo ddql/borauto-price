b04c2eb39c5b578be25b3642842c298c2150a304
ddql <52232549+ddql@users.noreply.github.com>
2023-05-16 10:29:59 +0300

    Update import.php

--- import.php
+++ import.php
@@ -1,68 +1,47 @@
 <!-- Здесь происходит выгрузка из хмл файла в sql запрос !-->
 <meta charset="utf-8">
 <?php
-include "db.php";                                               //подключаем бд
-$delete = "DELETE FROM car ";                                   //удаляем все строки
-$xml = simplexml_load_file('xml/price1.xml');                   //загружаем хмл файл
-                                                                //формируем запрос
-$query="
-INSERT INTO car (Code_DS, Sva_fio, type_purchase, Stock_days, manager_comment, 
-Planned_Profit, car_order_manager, diag_results, dop_price, Autoinspect_ID, def_ved, autoteka_url, 
-mark_id, folder_id, modification_id, body_type, drive, transmission, engine_volume, engine_power, engine_type, 
-date_admission, Owners, run, wheel, color_hex, color, availability, custom, state, year, price, credit_discount, 
-tradein_discount, insurance_discount, dealer_discount, registry_year, vin, car_order_exists, description, 
-extras, uploading_type) VALUES 
- ";
- $i=0;
-foreach ($xml->cars->car as $car)
+include "db.php";                                                               // подключаем бд
+$delete = "DELETE FROM car_list ";                                              // удаляем все строки
+mysqli_query($connectionDB, $delete) or die(mysqli_error($link));   
+$xml = simplexml_load_file('xml/test0.xml');                                    // загружаем хмл файл
+
+foreach ($xml->cars->car as $car)                                               // построчно импортируем в бд
 {
-    if ($i!=0) {$query.=", (";}
-    else {$query.="(";}
-    $query.="'".$car->Code_DS."', ";
-    $query.="'".$car->Sva_fio."', ";
-    $query.="'".$car->type_purchase."', ";
-    $query.="'".json_encode($car->Stock_days, JSON_UNESCAPED_UNICODE)."', ";
-    $query.="'".$car->manager_comment."', ";
-    $query.="'".$car->Planned_Profit."', ";
-    $query.="'".json_encode($car->car_order_manager, JSON_UNESCAPED_UNICODE)."', ";
-    $query.="'".json_encode($car->diag_results, JSON_UNESCAPED_UNICODE)."', ";
-    $query.="'".$car->dop_price."', ";
-    $query.="'".$car->Autoinspect_ID."', ";
-    $query.="'".json_encode($car->def_ved, JSON_UNESCAPED_UNICODE)."', ";
-    $query.="'".$car->autoteka_url."', ";
-    $query.="'".$car->mark_id."', ";
-    $query.="'".$car->folder_id."', ";
-    $query.="'".$car->modification_id."', ";
-    $query.="'".$car->body_type."', ";
-    $query.="'".$car->drive."', ";
-    $query.="'".$car->transmission."', ";
-    $query.="'".$car->engine_volume."', ";
-    $query.="'".$car->engine_power."', ";
-    $query.="'".$car->engine_type."', ";
-    $query.="'".$car->date_admission."', ";
-    $query.="'".$car->Owners."', ";
-    $query.="'".$car->run."', ";    
-    $query.="'".$car->wheel."', ";
-    $query.="'".$car->color_hex."', ";
-    $query.="'".$car->color."', ";
-    $query.="'".$car->availability."', ";
-    $query.="'".$car->custom."', ";
-    $query.="'".$car->state."', ";
-    $query.="'".$car->year."', ";
-    $query.="'".$car->price."', ";
-    $query.="'".$car->credit_discount."', ";
-    $query.="'".$car->tradein_discount."', ";
-    $query.="'".$car->insurance_discount."', ";
-    $query.="'".$car->dealer_discount."', ";
-    $query.="'".$car->registry_year."', ";
-    $query.="'".$car->vin."', ";
-    $query.="'".$car->car_order_exists."', ";
-    $query.="'".json_encode($car->description, JSON_UNESCAPED_UNICODE)."', ";
-    $query.="'".json_encode($car->extras, JSON_UNESCAPED_UNICODE)."', ";
-    $query.="'".$car->uploading_type."') ";
-    $i++;
-}
-echo $query;
-    mysqli_query($connectionDB, $delete) or die(mysqli_error($link));
+    $dataSet = [
+        "dc_name" => $car->dc_name,                                             // склад ДЦ
+        "vin" => $car->vin,                                                     // винкод
+        "mark_id" => $car->mark_id,                                             // марка
+        "folder_id" => $car->folder_id,                                         // модель
+        "modification_id" => $car->modification_id,                             // модификация            
+        "body_type" => $car->body_type,                                         // тип кузова
+        "transmission" => $car->transmission,                                   // тип кпп
+        "owners" => $car->owners,                                               // кво владельцев
+        "color" => $car->color,                                                 // цвет авито
+        "year" => $car->year,                                                   // год выпуска авто
+        "run" => $car->run,                                                     // пробег авто
+        "price" => $car->price,                                                 // розничная цена
+        "recommended_price" => $car->recommended_price,                         // РРЦ
+        "purchase_price" => $car->purchase_price,                               // цена закупки
+        "optional_equipment_price" => $car->optional_equipment_price,           // стоимость доп
+        "license_plate" => $car->license_plate,                                 // гос номер
+        "comment" => $car->comment,                                             // комментарий менеджера
+        "manager" => $car->manager,                                             // менеджер, который выкупал автомобиль
+        "stock_days" => $car->stock_days,                                       // кво дней на складе
+        "purchase_type" => $car->purchase_type,                                 // тип поступления на склад
+        "expenses_repair" => $car->expenses_repair,                             // сумма расходов на ремонт
+        "expenses_resources" => $car->expenses_resources,                       // сумма расходов на оборотку
+        "last_price_change" => $car->last_price_change,                         // количество дней с посл. переоценки
+        "autoteka_url" => $car->autoteka_url,                                   // ссылка на автотеку
+        "car_order_manager" => $car->car_order_manager,                         // заказ автомобиля(при наличии).менеджер
+        "vehiclemodification" => $car->vehiclemodification,                     // вариант комплектации
+        "extras" => $car->extras                                                // базовые опции
+        ];
+    $keys = join(',', array_keys($dataSet));
+    $values = join("','", array_values($dataSet));
+    $query="INSERT INTO car_list ($keys) VALUES ('$values')";
+    echo $query;
     mysqli_query($connectionDB, $query) or die(mysqli_error($link));   
+}
+
 ?>
