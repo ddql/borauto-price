b533b8444cd3ae10f57ee1722e0b2b42538ffaad
ddql <52232549+ddql@users.noreply.github.com>
2023-06-14 14:27:01 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -10,7 +10,7 @@ if(isset($_POST['submitLogin']))
     {   
     if ($_POST['inputLogin']=="1234" AND $_POST['inputLogin']=="1234"){
         setcookie("login", $_POST['inputLogin'], time()+60*60*24*30, "/");
-        setcookie("dc", $_POST['inputDC'], time()+60*60*24*30, "/");   
+        setcookie("dc", implode(',',$_POST['inputDC']), time()+60*60*24*30, "/"); 
         header('Location: '.$_SERVER['REQUEST_URI']); exit;
     }
     else{
@@ -26,29 +26,27 @@ if(isset($_POST['submitLogin']))
         <input name="inputLogin" type="text" id="inputLogin" placeholder="Логин" class="form-control">
         <label for="inputPassword" class="form-label">Пароль</label>
         <input name="inputPassword" type="password" id="inputPassword" placeholder="Пароль" class="form-control" >
-        <label class="form-label">Дилерский центр</label>
-        <select name="inputDC" class="form-select" required>
-            <option selected disabled value="">Выбор ДЦ</option>
-            <!--  Список складов !-->
-            <option>ДЦ 9 января_АМ_склад бу ам</option>
-            <option>ДЦ 9 января_АМ_склад новые ам</option>
-            <option>ДЦ Бк_АМ_склад бу ам</option>
-            <option>ДЦ Бк_АМ_склад новые ам</option>
-            <option>ДЦ Липецк_АМ_склад бу ам</option>
-            <option>ДЦ Липецк_АМ_склад новые ам</option>
-            <option>ДЦ Остужева_АМ_склад бу ам</option>
-            <option>ДЦ Остужева_АМ_склад новые ам</option>
-            <option>ДЦ Ростов_АМ_склад бу ам</option>
-            <option>ДЦ Ростов_АМ_склад новые ам</option>
-            <option>ДЦ СКС-Лада_АМ_склад бу ам</option>
-            <option>ДЦ СКС-Лада_АМ_склад новые ам</option>
-            <option>ДЦ Ставрополь_АМ_склад бу ам</option>
-            <option>ДЦ Ставрополь_АМ_склад новые ам Лада1</option>
-            <option>ДЦ Ставрополь_АМ_склад новые ам УАЗ</option>
-            <option>ДЦ Тамбов_АМ_склад бу ам</option>
-            <option>ДЦ Тамбов_АМ_склад новые ам</option>
-            
-        </select>
+        <label class="form-label">Склад</label>
+           <select name="inputDC[]" class="form-select" multiple="multiple">
+                <option>ДЦ 9 января_АМ_склад бу ам</option>
+                <option>ДЦ 9 января_АМ_склад новые ам</option>
+                <option>ДЦ Бк_АМ_склад бу ам</option>
+                <option>ДЦ Бк_АМ_склад новые ам</option>
+                <option>ДЦ Липецк_АМ_склад бу ам</option>
+                <option>ДЦ Липецк_АМ_склад новые ам</option>
+                <option>ДЦ Остужева_АМ_склад бу ам</option>
+                <option>ДЦ Остужева_АМ_склад новые ам</option>
+                <option>ДЦ Ростов_АМ_склад бу ам</option>
+                <option>ДЦ Ростов_АМ_склад новые ам</option>
+                <option>ДЦ СКС-Лада_АМ_склад бу ам</option>
+                <option>ДЦ СКС-Лада_АМ_склад новые ам</option>
+                <option>ДЦ Ставрополь_АМ_склад бу ам</option>
+                <option>ДЦ Ставрополь_АМ_склад новые ам Лада1</option>
+                <option>ДЦ Ставрополь_АМ_склад новые ам УАЗ</option>
+                <option>ДЦ Тамбов_АМ_склад бу ам</option>
+                <option>ДЦ Тамбов_АМ_склад новые ам</option>
+                <option>ДЦ Аксай_АМ_склад бу ам</option>
+            </select>
         <br> 
         <button name="submitLogin" type="submit" type='button' class='btn btn-light'>Войти</button>
     </div>
